5dea9c7f3d1cfcda059d9e153717cb268cbf8f7e
Rauf Abbas <32704040+rafoabbas@users.noreply.github.com>
2018-10-26 12:28:33 -0700

    First Comit
    Composer and ServiceProvider create

---
+++ src/Providers/InstallerServiceProvider.php
@@ -0,0 +1,29 @@
+<?php
+/**
+ * Created by Kubpro.com.
+ * Devloper: Rauf Abbas
+ * Date: 10/26/2018
+ * Time: 12:27 PM
+ * Website: Kubpro.com
+ */
+
+namespace Kubpro\Contact\Providers;
+
+
+use Illuminate\Support\ServiceProvider;
+
+class InstallerServiceProvider extends ServiceProvider
+{
+
+    public function boot()
+    {
+
+    }
+
+
+    public function register()
+    {
+
+    }
+
+}
