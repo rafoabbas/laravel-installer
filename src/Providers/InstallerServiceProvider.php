98120f99403fc2669eb5608934b18e452088ff97
Rauf Abbas <32704040+rafoabbas@users.noreply.github.com>
2018-10-26 12:34:49 -0700

    Composer And Service Providers

--- src/Providers/InstallerServiceProvider.php
+++ src/Providers/InstallerServiceProvider.php
@@ -7,19 +7,29 @@
  * Website: Kubpro.com
  */
 
-namespace Kubpro\Contact\Providers;
+namespace Kubpro\Installer\Providers;
 
 
 use Illuminate\Support\ServiceProvider;
 
 class InstallerServiceProvider extends ServiceProvider
 {
+    /**
+     * Perform post-registration booting of services.
+     *
+     * @return void
+     */
 
     public function boot()
     {
-
+        $this->loadRoutesFrom(__DIR__."./../Routes/web.php");
     }
 
+    /**
+     * Register bindings in the container.
+     *
+     * @return void
+     */
 
     public function register()
     {
